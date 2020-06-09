b659a56bd6989b896544c04db8225e3073126f28
Vitaliy Ponomaryov <vital77766688@yandex.ru>
2020-06-09 19:21:10 +0600

    Improved styling

--- scripts.js
+++ scripts.js
@@ -1,16 +1,50 @@
-const main_burger = document.querySelector('.main-burger')
+const burger = document.querySelector('.burger')
 const side_menu = document.querySelector('.side-menu')
+const side_menu_children = Object.values(side_menu.getElementsByTagName("*"))
+const body = document.querySelector('body')
+const category_titles = document.querySelectorAll('.category-title')
 
-main_burger.addEventListener('click', () => {
-	main_burger.classList.toggle('active')
+
+function getActiveCategory() {
+	category_titles.forEach(category_title => {
+		const top = category_title.getBoundingClientRect().top
+		const active_category = side_menu.querySelector(`a[href="#${category_title.parentNode.id}"]`)
+		if (parseInt(top) === 0) {
+			active_category.classList.add('active')
+		} else {
+			active_category.classList.remove('active')
+		}
+	})
+}
+
+getActiveCategory()
+
+
+for (let elem of side_menu_children) {
+	if (elem.classList.contains('category-link')) {
+		side_menu_children.splice(side_menu_children.indexOf(elem), 1)
+	}
+}
+
+burger.addEventListener('click', () => {
+	burger.classList.toggle('active')
 	side_menu.classList.toggle('active')
+	body.classList.toggle('lock')
 })
 
 
 window.addEventListener('click', e => {
-	console.log(e.target, e.target.parentNode)
-	// if (e.target != side_menu && e.target.parentNode != side_menu && e.target != main_burger) {
-	// 	main_burger.classList.remove('active')
-	// 	side_menu.classList.remove('active')
-	// }
+	if (e.target != burger
+		&& !Object.values(burger.children).includes(e.target)
+		&& e.target != side_menu 
+		&& !side_menu_children.includes(e.target)) {
+		body.classList.remove('lock')
+		burger.classList.remove('active')
+		side_menu.classList.remove('active')
+	}
+})
+
+
+window.addEventListener('scroll', () => {
+	getActiveCategory()
 })
